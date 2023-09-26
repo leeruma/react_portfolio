3efae32cdd18c875cf04acccfcb4ef7930651335
leeruma <achieve4525@naver.com>
2023-09-26 13:39:11 +0900

    department sub디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/Global.scss';
 import Header from './components/common/header/Header';
+import Department from './components/sub/department/Department';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Department />
 		</>
 	);
 }
